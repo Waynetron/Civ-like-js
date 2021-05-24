c610e80662934fd3c5520dcf9dc8a89a40734133
Waynetron <wpetzler@gmail.com>
2021-05-24 14:27:37 +1000

    Swapped from two.js to paper.js

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from "react";
-import { useDrag } from "react-use-gesture";
-import Two from "two.js";
+import Paper from "paper";
 import "./Map.css";
 
 const NUM_COLS = 10;
@@ -8,20 +7,41 @@ const NUM_ROWS = 20;
 const HEX_RADIUS = 50;
 
 const X_SPACING = HEX_RADIUS * 3;
-const Y_SPACING = HEX_RADIUS * 0.85;
+const Y_SPACING = HEX_RADIUS * Math.sin((60 * Math.PI) / 180);
 const ODD_ROW_OFFSET = HEX_RADIUS * 1.5;
 const MAP_WIDTH = NUM_COLS * X_SPACING;
 const MAP_HEIGHT = NUM_ROWS * Y_SPACING;
 
+const makeHex = (x, y, radius) => {
+  const hex = new Paper.Path.RegularPolygon({
+    center: [x, y],
+    sides: 6,
+    radius: radius,
+    strokeColor: "black",
+    fillColor: "white",
+    strokeWidth: 3,
+    rotation: 90,
+  });
+
+  console.log(hex);
+
+  hex.onMouseEnter = function (event) {
+    this.fillColor = "red";
+  };
+
+  hex.onMouseLeave = function (event) {
+    this.fillColor = "white";
+  };
+
+  return hex;
+};
+
 function Map() {
-  const divRef = useRef();
-  const two = useRef();
+  const canvasRef = useRef();
 
   useEffect(() => {
-    two.current = new Two({
-      fullscreen: true,
-      autostart: true,
-    }).appendTo(divRef.current);
+    const canvas = canvasRef.current;
+    Paper.setup(canvas);
 
     const hexagons = [];
     const x = 0 - MAP_WIDTH / 2;
@@ -30,44 +50,30 @@ function Map() {
     for (let row = 0; row < NUM_ROWS; row++) {
       const isOddRow = row % 2 === 0;
       for (let col = 0; col < NUM_COLS; col++) {
-        const hex = two.current.makePolygon(
+        const hex = makeHex(
           x + col * X_SPACING + (isOddRow ? ODD_ROW_OFFSET : 0),
           y + row * Y_SPACING,
-          HEX_RADIUS,
-          6
+          HEX_RADIUS
         );
+
         hexagons.push(hex);
       }
     }
 
-    const group = two.current.makeGroup(...hexagons);
-    two.current.bind("update", function () {
-      // group.rotation += 0.001;
-    });
+    const group = new Paper.Group(...hexagons);
+    group.translate(
+      new Paper.Point(Paper.view.bounds.width / 2, Paper.view.bounds.height / 2)
+    );
 
-    two.current.scene.stroke = "#232323";
-    two.current.scene.linewidth = 3;
+    Paper.view.onMouseDrag = (event) => {
+      group.translate(event.delta);
+      // myPath.add(event.point);
+    };
 
-    // group.center();
-    group.translation.set(two.current.width / 2, two.current.height / 2);
+    Paper.view.draw();
   }, []);
 
-  const bind = useDrag(
-    ({ offset: [x, y] }) => {
-      two.current.scene.translation.set(x, y);
-    },
-    {
-      bounds: {
-        left: -MAP_WIDTH / 2,
-        right: MAP_WIDTH / 2,
-        top: -MAP_HEIGHT / 2,
-        bottom: MAP_HEIGHT / 2,
-      },
-      // rubberband: true,
-    }
-  );
-
-  return <div ref={divRef} className="Map" {...bind()} className="map"></div>;
+  return <canvas ref={canvasRef} id="canvas" resize="true" />;
 }
 
 export default Map;
