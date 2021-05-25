6807743d5222513dd655ce2d5dd9092e49fde012
Waynetron <wpetzler@gmail.com>
2021-05-25 22:38:03 +1000

    Refactor preload images

--- src/Map/map-logic.js
+++ src/Map/map-logic.js
@@ -2,19 +2,7 @@ import Paper from "paper";
 import { makeTile, hover, select, deselect, deselectAll } from "../Tile/tile";
 import colors from "../Util/colors";
 import { NUM_COLS, NUM_ROWS } from "./map-constants";
-import forestSVG from "../Tile/images/forest.svg";
-import castleSVG from "../Tile/images/castle.svg";
-import mountainSVG from "../Tile/images/mountain.svg";
-import num1SVG from "../Tile/images/num1.svg";
-import num2SVG from "../Tile/images/num2.svg";
-import num3SVG from "../Tile/images/num3.svg";
-import num4SVG from "../Tile/images/num4.svg";
-import num5SVG from "../Tile/images/num5.svg";
-import num6SVG from "../Tile/images/num6.svg";
-import skeletonSVG from "../Tile/images/skeleton.svg";
-import skeletonSelectedSVG from "../Tile/images/skeleton-selected.svg";
-import rabbitSVG from "../Tile/images/rabbit.svg";
-import rabbitSelectedSVG from "../Tile/images/rabbit-selected.svg";
+import preloadImages from "./preload-images";
 
 let hexGroup;
 let imageGroup;
@@ -31,42 +19,13 @@ const makeTiles = function (images) {
   }
   return newTiles;
 };
-
-export const initMap = function (canvas, setTiles, setSelected) {
+export const initMap = function (canvas, setTiles, setSelected, units) {
   Paper.setup(canvas);
-
-  // preload the SVG images
-  const imagesToLoad = {
-    forest: forestSVG,
-    castle: castleSVG,
-    mountain: mountainSVG,
-    num1: num1SVG,
-    num2: num2SVG,
-    num3: num3SVG,
-    num4: num4SVG,
-    num5: num5SVG,
-    num6: num6SVG,
-    skeleton: skeletonSVG,
-    skeletonSelected: skeletonSelectedSVG,
-    rabbit: rabbitSVG,
-    rabbitSelected: rabbitSelectedSVG,
-  };
-  const images = {};
-  for (const [type, svg] of Object.entries(imagesToLoad)) {
-    Paper.project.importSVG(svg, {
-      onLoad: (image) => {
-        images[type] = image;
-        if (
-          Object.entries(images).length === Object.entries(imagesToLoad).length
-        ) {
-          console.log("loaded");
-          generateMap(setTiles, setSelected, images);
-        }
-      },
-      insert: false,
-    });
-  }
+  preloadImages({
+    onComplete: (images) => generateMap(setTiles, setSelected, images),
+  });
 };
+
 const generateMap = function (setTiles, setSelected, images) {
   hexGroup = new Paper.Group();
   imageGroup = new Paper.Group();
